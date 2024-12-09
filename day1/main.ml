dc8560f59c6ac7672b05ede9b241045ee423c776
James Meyers <james.meyers212@gmail.com>
2024-12-08 21:05:42 -0500

    aoc 2024

---
+++ day1/main.ml
@@ -0,0 +1,19 @@
+let answer l1 l2 = 
+  List.combine (List.sort compare l1) (List.sort compare l2)
+  |> List.map (fun (x, y) -> abs (x - y))
+  |> List.fold_left (+) 0
+
+let parse_input s = 
+  s |> String.split_on_char '\n' 
+    |> List.map (fun s -> String.split_on_char ' ' s |> List.filter (fun s -> s <> "") |> List.map int_of_string)
+    |> List.map (fun l -> List.hd l, List.hd (List.tl l))
+    |> List.split
+
+let () = 
+  let l1, l2 = parse_input "3   4
+4   3
+2   5
+1   3
+3   9
+3   3" in 
+  answer l1 l2 |> string_of_int |> print_endline
